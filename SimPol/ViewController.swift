3f00ece5b01c34069a6bfe3b3843cce749c7a5b8
Josh Gelbard <gelbard@wustl.edu>
2016-04-17 13:21:53 -0500

    Initial Commit

---
+++ SimPol/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimPol
+//
+//  Created by Gelbard on 4/17/16.
+//  Copyright © 2016 Joshua Gelbard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
